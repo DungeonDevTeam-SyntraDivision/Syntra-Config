35e952ca3af424acb28d8da0bae0a7921527522f
alaricespina <alaricsespina@gmail.com>
2026-02-09 07:14:00 +0000

    Fix Sudo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp hatdog.service /etc/systemd/system/hatdog.service
+sudo cp hatdog.service /etc/systemd/system/hatdog.service
 chmod +x start.sh
 sudo systemctl daemon-reload
 sudo systemctl enable hatdog
