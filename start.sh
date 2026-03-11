438131904b79ef37819a699e0635a761d53f89fd
Alaric <alaricsespina@gmail.com>
2026-03-11 23:51:22 +0800

    1

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Starting hotspot and docker containers..."
 sudo hotspot-on
 docker compose up -d
 sleep 15 
-source ~/Documents/Syntra-Config/.venv/bin/activate
-python3 ~/Documents/Syntra-Config/start.py
+source .venv/bin/activate
+python3 start.py
