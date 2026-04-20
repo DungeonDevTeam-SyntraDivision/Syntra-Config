537d751b8445568b5e00da88a5153449c6489eef
Alaric S. Espiña <alaricsespina@gmail.com>
2026-04-20 14:35:42 +0800

    Prod

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sleep 15
 echo "Starting hotspot and docker containers..."
+sudo hotspot-off
 sudo hotspot-on
 docker compose up -d
-sleep 15 
-source /home/syntra/Documents/Syntra-Config/.venv/bin/activate
-python3 /home/syntra/Documents/Syntra-Config/start.py
+sleep 15
+sudo -u syntra DISPLAY=:0 XAUTHORITY=/home/syntra.XAuthority .venv/bin/python3 /home/syntra/Documents/Syntra-Config/start.py
